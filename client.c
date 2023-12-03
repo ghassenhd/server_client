88f3a6e8ea281f0c263f5a78805eda9bcb6681aa
ghassenhd <94930743+ghassenhd@users.noreply.github.com>
2023-12-03 19:43:02 +0100

    Implement the client

--- client.c
+++ client.c
@@ -1,6 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#define DEFAULT_PORT                (1999)
+#define SERV_ADDR_MAX_LEN           (16)
+#define DEFAULT_SERVER_ADDRESS      "127.0.0.1"
+#define MAX_DATA_RECV               (100)
+
+#define CONNECTION_ERROR  (1)
+#define OPTION_ERROR      (-1)
+#define SUCCES            (0)
+
+#define DISPLAY(...)      {printf("[%s:%s:%d]", __FILE__, __func__, __LINE__);printf(__VA_ARGS__);}
+
 int main(int argc, char ** argv)
 {
 
+  int  port=DEFAULT_PORT, sock_fd, option='?';
+  char str_serv_address[SERV_ADDR_MAX_LEN];
+  snprintf(str_serv_address,strlen(DEFAULT_SERVER_ADDRESS)+1,DEFAULT_SERVER_ADDRESS);
 
-  return 0;
+  while ((option = getopt(argc, argv, ":p:a:h"))!=-1)
+  {
+    switch (option)
+    {
+      case 'a':
+        snprintf(str_serv_address,strlen(optarg)+1,"%s",optarg);
+        break;
+      case 'p':
+        port = atoi(optarg);
+        break;
+      case 'h':
+        printf("%s [-p <port> | -h | -a <server address>]\n", argv[0]);
+        return SUCCES;
+      case '?':
+        printf("wrong options\n");
+      default :
+        printf("%s [-p <port> | -h | -a <server address>]\n", argv[0]);
+        return OPTION_ERROR;
+    }
+  }
+  
+  if((sock_fd = socket(AF_INET,SOCK_STREAM, 0))==-1)
+  {
+    DISPLAY("error : socket() %d\n",errno)
+    return CONNECTION_ERROR;
+  }
+  DISPLAY("Create socket: OK\n")
+
+  struct sockaddr_in server_address={AF_INET,  htons(port)};
+  inet_pton(AF_INET, str_serv_address, &server_address.sin_addr);
+  memset(&(server_address.sin_zero), 0, 8);
+  if (connect(sock_fd, (struct sockaddr*)&server_address, sizeof(server_address)) == -1)
+  {
+    DISPLAY("error : connect() %d\n",errno)
+    return CONNECTION_ERROR;
+  }
+  DISPLAY("Connect: OK\n")
+
+  int num_bytes;
+  char data_recv[MAX_DATA_RECV];
+  while(num_bytes = recv(sock_fd, data_recv, MAX_DATA_RECV-1, 0))
+  {
+    if (num_bytes == -1)
+    {
+      DISPLAY("error : recv() %d\n",errno)
+      break;
+    }
+
+    if(!num_bytes)
+    {
+      DISPLAY("Server closed the connection\n")
+      break;
+    }
+
+    data_recv[num_bytes] = '\0';
+    DISPLAY("Client received: %s\n", data_recv)
+  }
+  
+  close(sock_fd);
+  DISPLAY("Socket closed\n")
+
+  return SUCCES;
 }
--- server.c
+++ server.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <errno.h>
 
+#define MAX_DATA_SEND               (100)
 #define MAX_NUM_CLIENTS   (1000)
 #define DEFAULT_PORT      (1999)
 #define BACKLOG           (512 ) //shoud not be bigger then the value 
                                  //in "/proc/sys/net/ipv4/tcp_max_syn_backlog".
 
-#define OPTION_ERROR    (-1)
-#define SUCCES          (0)
+#define CONNECTION_ERROR  (1)
+#define OPTION_ERROR      (-1)
+#define SHARED_DATA_ERROR (2)
+#define SUCCES            (0)
 
 #define DISPLAY(...)      {printf("[%s:%s:%d]", __FILE__, __func__, __LINE__);printf(__VA_ARGS__);}
 
@@ -36,8 +37,8 @@ void* handle_client(void* client_data)
   int connection_id = data->connection_id;
   int client_fd = data->client_fd;
   pthread_t  thread_id= data->thread_id;
-  char message[1024];
-  snprintf(message,1024,"Hello client %d",connection_id);
+  char message[MAX_DATA_SEND];
+  snprintf(message, MAX_DATA_SEND-1,"Hello client %d",connection_id);
   if (send(client_fd,message,strlen(message), 0) == -1)
     DISPLAY("error : send() %d\n",errno) 
 
@@ -45,6 +46,7 @@ void* handle_client(void* client_data)
   nb_clients--;
   pthread_mutex_unlock(&lock);
   close(client_fd);
+  DISPLAY("Socket is closed for connection %d\n", connection_id)
   sem_post(&sema);
   free(data);
   return NULL;
@@ -55,7 +57,7 @@ int main(int argc, char ** argv)
 {
   int port=DEFAULT_PORT, max_clients=MAX_NUM_CLIENTS, option='?';
   int sock_fd, new_fd, sin_size;
-  struct sockaddr_in my_addr, their_addr;
+  struct sockaddr_in my_addr, client_addr;
 
   while ((option = getopt(argc, argv, ":p:hn:"))!=-1)
   {
@@ -81,8 +83,10 @@ int main(int argc, char ** argv)
   if((sock_fd = socket(AF_INET,SOCK_STREAM, 0))==-1)
   {
     DISPLAY("error : socket() %d\n",errno)
-    return -1;
+    return CONNECTION_ERROR;
   }
+  DISPLAY("Create socket: OK\n")
+
   my_addr.sin_family = AF_INET;
   my_addr.sin_port = htons(port);
   my_addr.sin_addr.s_addr = INADDR_ANY;
@@ -91,27 +95,31 @@ int main(int argc, char ** argv)
   if (bind(sock_fd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)))
   {
     DISPLAY("error : bind() %d\n",errno)
-    return -1;
+    return CONNECTION_ERROR;
   }
+  DISPLAY("Bind: OK\n")
+  
   if (listen(sock_fd, BACKLOG))
   {
     DISPLAY("error : listen() %d\n",errno)
-    return -1;
+    return CONNECTION_ERROR;
   }
-
+  DISPLAY("Listen: OK\n")
+  
   if(sem_init(&sema, 0, max_clients))
   {
     DISPLAY("error : sem_init() %d\n",errno)
-    return -1;
+    return SHARED_DATA_ERROR;
   }
   if (pthread_mutex_init(&lock, NULL))
   {
     DISPLAY("error : pthread_mutex_init()\n")
-    return -1;
+    return SHARED_DATA_ERROR;
   }
 
   int connection_id=0;
   struct ClientData * client_data=NULL;
+  char * thread_error="server couldn't create a thread for the client\n";
   while (1)
   {
     if(sem_wait(&sema))
@@ -119,25 +127,28 @@ int main(int argc, char ** argv)
       DISPLAY("error : sem_wait() %d\n",errno)
       continue;
     }  
-    new_fd = accept(sock_fd, (struct sockaddr*)&their_addr, &sin_size);
+    new_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &sin_size);
     if (new_fd == -1)
     {
       DISPLAY("error : accept()\n")
       sem_post(&sema);
       continue;
-    } 
+    }
+    DISPLAY("Establishe connection %d: OK\n", ++connection_id)
+
     client_data = (struct ClientData*)malloc(sizeof(struct ClientData));
     client_data->client_fd=new_fd;
-    client_data->connection_id=++connection_id;
+    client_data->connection_id=connection_id;
     if(pthread_create(&(client_data->thread_id),NULL,handle_client,(void*)client_data))
     {
       DISPLAY("error : pthread_create()\n")
       sem_post(&sema);
-      if (send(new_fd, "server couldn't create a thread for the client\n", 47, 0) == -1)
+      if (send(new_fd, thread_error, strlen(thread_error)+1, 0) == -1)
         DISPLAY("error : send()\n")
       close(new_fd);
       continue;
     }
+    DISPLAY("Create thread for connection %d: OK\n", connection_id)
     pthread_detach(client_data->thread_id);
     pthread_mutex_lock(&lock);
     nb_clients++;
